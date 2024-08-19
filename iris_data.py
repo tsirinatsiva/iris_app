64eead258cb63bfea56d60eb58427a1f2b5ab711
tsirinatsiva <145757693+tsirinatsiva@users.noreply.github.com>
2024-08-19 08:58:00 +0300

    Update iris_data.py

--- iris_data.py
+++ iris_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.figure_factory as ff
+# import plotly.express as px
+# import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 from urllib.request import urlopen
 import streamlit as st
