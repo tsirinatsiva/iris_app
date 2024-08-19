1e09c4b1f85a5c50a6aed173e06a814cced74e89
tsirinatsiva <145757693+tsirinatsiva@users.noreply.github.com>
2024-08-19 08:54:50 +0300

    Update iris_data.py

--- iris_data.py
+++ iris_data.py
@@ -18,28 +18,28 @@ iris.columns=col
 st.dataframe(iris)
 
 
-fig = px.scatter_matrix(iris,dimensions=['sepal_length','sepal_width','petal_length','petal_width'],color='iris_type')
-st.plotly_chart(fig,theme='streamlit')
+# fig = px.scatter_matrix(iris,dimensions=['sepal_length','sepal_width','petal_length','petal_width'],color='iris_type')
+# st.plotly_chart(fig,theme='streamlit')
 
-#FILTER BY IRIS SPECIES
-species = st.selectbox('SPECIES',list(iris['iris_type'].unique()))
+# #FILTER BY IRIS SPECIES
+# species = st.selectbox('SPECIES',list(iris['iris_type'].unique()))
 
-#IRIS AFTER FILTERING
-iris_filtered = iris[iris['iris_type']==species]
+# #IRIS AFTER FILTERING
+# iris_filtered = iris[iris['iris_type']==species]
 
-hist_data = [iris_filtered['sepal_length'],iris['sepal_width'],iris['petal_length'],iris['petal_width']]
-group_labels = ['sepal_length','sepal_width','petal_length','petal_width']
+# hist_data = [iris_filtered['sepal_length'],iris['sepal_width'],iris['petal_length'],iris['petal_width']]
+# group_labels = ['sepal_length','sepal_width','petal_length','petal_width']
 
-st.markdown('## Distribution Plot per features')
-fig_2 = ff.create_distplot(hist_data, group_labels, bin_size = [.25,.25,.25,0.25])
+# st.markdown('## Distribution Plot per features')
+# fig_2 = ff.create_distplot(hist_data, group_labels, bin_size = [.25,.25,.25,0.25])
 
 
-st.plotly_chart(fig_2,theme = None)
+# st.plotly_chart(fig_2,theme = None)
 
-st.markdown('##  Distribution per species')
+# st.markdown('##  Distribution per species')
 
-col_select = ['sepal_length','sepal_width','petal_length','petal_width']
-feat_select = st.selectbox('FEATURES',col_select)
+# col_select = ['sepal_length','sepal_width','petal_length','petal_width']
+# feat_select = st.selectbox('FEATURES',col_select)
 
-fig_3=px.box(iris,'iris_type',feat_select)
-st.plotly_chart(fig_3)
+# fig_3=px.box(iris,'iris_type',feat_select)
+# st.plotly_chart(fig_3)
